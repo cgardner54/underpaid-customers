d12d6bf623102f854dac8c4e6447f72028c73931
Christina <crissygardner@gmail.com>
2021-01-07 05:06:53 +0000

    finished function

--- new_report.py
+++ new_report.py
@@ -10,14 +10,11 @@ def parselogs(path):
         customer_name = words[1]
         melons = words[2]
         amount_paid = words[3]
-        overpaid_underpaid = float(amount_paid) - float(melons)
+        overpaid_underpaid = float(melons) - float(amount_paid)
 
-        print()
-
-customer1_expected = customer1_melons * melon_cost
-if customer1_expected != customer1_paid:
-    print(f"{customer1_name} paid ${customer1_paid:.2f},",
-          f"expected ${customer1_expected:.2f}"
-          )
-
+        if overpaid_underpaid > 0:
+            print(f"{customerid} | {customer_name} underpaid by ${overpaid_underpaid:.2f}.")
+        else:
+            ""
+            
 parselogs('customer-orders.txt')
